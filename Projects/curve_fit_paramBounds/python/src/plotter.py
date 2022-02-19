c927e680e3fa95fda3b155914378102b5b204dbb
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-02-19 20:07:10 +0200

    changed the plot styling

--- Projects/curve_fit_paramBounds/python/src/plotter.py
+++ Projects/curve_fit_paramBounds/python/src/plotter.py
@@ -14,11 +14,21 @@ def comparisonPlot(param_set1, param_set2):
     y_data_1 = [model.model_function(x, p11, p12, p13) for x in x_data]
     y_data_2 = [model.model_function(x, p21, p22, p23) for x in x_data]
 
-    plt.plot(x_data, y_data_1, 'ok', label='exp.')
-    plt.plot(x_data, y_data_1, '-r', linewidth=2, label='th.')
+    plt.plot(x_data, y_data_1, 'ok', markersize=3, label='exp.')
+    plt.plot(x_data, y_data_2, '-r', linewidth=1.5, label='th.')
     plt.legend(loc='best')
     plt.xlabel('x')
     plt.ylabel('f(x)')
     plt.tight_layout()
     plt.savefig('plot_comparison.pdf', dpi=300, bbox_inches='tight')
     plt.close()
+
+
+def main():
+    param_set1 = [1, 2, 3]
+    param_set2 = [1.1, 2.2, 3.3]
+    comparisonPlot(param_set1, param_set2)
+
+
+if __name__ == '__main__':
+    main()
