52230b0b6ffde87896e17eccd0aa499e7b1303b1
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-02-20 20:12:46 +0200

    create exp.th data from params

---
+++ Projects/input_data_multiple_args/python/src/model.py
@@ -0,0 +1,68 @@
+import random as rd
+
+
+def generate_x_data(size, phonon1, phonon2, even_state):
+    phonon = lambda: rd.choice([0, 1])
+
+    if(even_state == 1):
+        s0 = 2.5
+    else:
+        s0 = 3.5
+
+    x_data = [(s0 + 2 * k, phonon1, phonon2) for k in range(size)]
+
+    return x_data
+
+
+def create_experimental_data(size):
+    even_data = 'band1.dat'
+    odd_data = 'band2.dat'
+
+    even_spins = generate_x_data(size, 0, 0, 1)  # zero phonon states
+    odd_spins = generate_x_data(size, 1, 0, 0)  # one-phonon wobbling states
+
+    with open(even_data, 'w+') as f:
+        for I_even in even_spins:
+            f.write(str(I_even[0]) + ' ' +
+                    str(I_even[1]) + ' ' + str(I_even[2]))
+            f.write('\n')
+
+    with open(odd_data, 'w+') as f:
+        for I_odd in odd_spins:
+            f.write(str(I_odd[0]) + ' ' +
+                    str(I_odd[1]) + ' ' + str(I_odd[2]))
+            f.write('\n')
+
+    return [even_spins, odd_spins]
+
+
+def model_function(X, a, b, c):
+    spin, phonon1, phonon2 = X
+
+    f = a * pow(spin, 3) * (phonon1 + 0.5) + b * \
+        pow(spin, 2) * (phonon2 + 0.5) + c * spin
+
+    return f
+
+
+def generate_data_from_params(size, params):
+    even_spins, odd_spins = create_experimental_data(size)
+    band1 = [w for w in even_spins]
+    band2 = [w for w in odd_spins]
+
+    p1, p2, p3 = params
+
+    model_band_1 = [model_function(x, p1, p2, p3) for x in band1]
+    model_band_2 = [model_function(x, p1, p2, p3) for x in band2]
+
+    print(model_band_1)
+    print(model_band_2)
+
+
+def main():
+    test_params = [1, 2, 0]
+    generate_data_from_params(15, test_params)
+
+
+if __name__ == '__main__':
+    main()
