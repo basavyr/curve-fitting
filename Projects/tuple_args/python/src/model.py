fix the numerical data with [arrays

Projects/tuple_args/python/src/model.py
@@ -3,6 +3,8 @@ import numpy as np
 from scipy.optimize import curve_fit
 import random as rd
 
+import plotter
+
 
 def model_function(X, a, b, c, d):
     """
@@ -12,9 +14,8 @@ def model_function(X, a, b, c, d):
 
     f = a * (pow(spins, 5)) * (even_phonons + 0.5) + \
         b * (pow(spins, 3)) * (odd_phonons + 0.5) + c * d * spins * constants
-    f2 = np.round(spins + even_phonons)
 
-    return f, f2
+    return f
 
 
 def generate_x_data(size):
@@ -23,7 +24,6 @@ def generate_x_data(size):
     odd_phonons = [0 if x == 1 else 1 for x in even_phonons]
     constants = [rd.randint(1, 10) for _ in range(len(spins))]
 
-    # tupled_items = (spins, even_phonons, odd_phonons, constants)
     tupled_items = np.array([spins, even_phonons, odd_phonons, constants])
 
     return tupled_items
@@ -34,8 +34,10 @@ def main():
 
     test_params = [0.3, 1, 2, 3, 4]
 
-    print(model_function(
-        X, test_params[0], test_params[1], test_params[2], test_params[3]))
+    MF = model_function(
+        X, test_params[0], test_params[1], test_params[2], test_params[3])
+
+    plotter.plot_function(X[0], MF)
 
 
 if __name__ == '__main__':