bfda1c6723b9066c7ab37000d2c95c3720b847d5
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-02-21 20:19:29 +0200

    add function model with two arrays as X

--- Projects/tuple_args/python/src/model.py
+++ Projects/tuple_args/python/src/model.py
@@ -8,10 +8,12 @@ def model_function(X, a, b, c, d):
     """
     - X is a tuple of arrays 
     """
-    spins, even_phonons, odd_phonons, constants = X
+    # spins, even_phonons, odd_phonons, constants = X
+    spins, even_phonons = X
 
-    f = a * (pow(spins, 5)) * (even_phonons + 0.5) + \
-        b * (pow(spins, 3)) * (odd_phonons + 0.5) + c * d * spins * constants
+    # f = a * (pow(spins, 5)) * (even_phonons + 0.5) + \
+    #     b * (pow(spins, 3)) * (odd_phonons + 0.5) + c * d * spins * constants
+    f = np.round(a * pow(spins, 5) * even_phonons + 1, 3)
 
     return f
 
@@ -19,16 +21,23 @@ def model_function(X, a, b, c, d):
 def generate_x_data(size):
     spins = [idx for idx in range(0, 2 * size, 2)]
     even_phonons = [rd.choice([0, 1]) for _ in range(len(spins))]
-    odd_phonons = [0 if x == 1 else 1 for x in even_phonons]
-    constants = [rd.randint(1, 10) for _ in range(len(spins))]
+    # odd_phonons = [0 if x == 1 else 1 for x in even_phonons]
+    # constants = [rd.randint(1, 10) for _ in range(len(spins))]
 
-    tupled_items = (spins, even_phonons, odd_phonons, constants)
+    # tupled_items = (spins, even_phonons, odd_phonons, constants)
+    tupled_items = np.array([spins, even_phonons])
 
     return tupled_items
 
 
 def main():
-    print(generate_x_data(8))
+    X = generate_x_data(8)
+    print(X)
+
+    test_params = [0.3, 1, 2, 3, 4]
+
+    print(model_function(
+        X, test_params[0], test_params[1], test_params[2], test_params[3]))
 
 
 if __name__ == '__main__':
